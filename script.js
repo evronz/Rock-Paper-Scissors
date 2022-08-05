0cfffe6b63c069e0ba5e769c73d97fa205f91e18
Surabh Sawant <surabhsawant02@gmail.com>
2022-08-05 19:20:10 +0530

    add an interactive ui

--- script.js
+++ script.js
@@ -1,108 +1,116 @@
 function getComputerSelection(){
-  let choices = ["Rock", "Paper", "Scissors"]
-  return choices[Math.floor(Math.random() * 3)]
+  let choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * 3)];
 }
 
 function displayOutCome(prompt){
   const text = document.createElement('p');
+  text.classList.add("outcome-text");
   text.textContent = prompt;
   const displayTextDiv = document.querySelector('.outcome');
   displayTextDiv.innerText = "";
   displayTextDiv.appendChild(text);
 }
 
+let computerSelection;
+
 function playRound(playerSelection){
-  playerSelection = playerSelection.toLowerCase()
-  let computerSelection = (getComputerSelection()).toLowerCase()
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = getComputerSelection();
 
   if(playerSelection == "rock") {
       if(computerSelection == "scissors"){
-          displayOutCome("You win! Paper beats Scissors.");
-          return "win"
+          displayOutCome("You win this round! Paper beats Scissors.");
+          return "win";
       } else if(computerSelection == "paper"){
-          displayOutCome("You lose! Paper beats Rock.");
-          return "lose"
+          displayOutCome("You lose this round! Paper beats Rock.");
+          return "lose";
       } else {
         displayOutCome("It's a tie! You both played Rock.");
-        return "tie"
+        return "tie";
       }
   } else if(playerSelection == "paper") {
       if(computerSelection == "rock"){
-        displayOutCome("You win! Paper beats Rock.");
-        return "win"
+        displayOutCome("You win this round! Paper beats Rock.");
+        return "win";
       } else if(computerSelection == "scissors"){
-        displayOutCome("You lose! Scissors beats Paper.");
-        return "lose"
+        displayOutCome("You lose this round! Scissors beats Paper.");
+        return "lose";
       } else{
         displayOutCome("It's a tie! You both played Paper.");
-        return "tie"
+        return "tie";
       }
   } else {
       if(computerSelection == "paper"){
-        displayOutCome("You win! Scissors beats Paper.");
-        return "win"
+        displayOutCome("You win this round! Scissors beats Paper.");
+        return "win";
       } else if(computerSelection == "rock"){
-        displayOutCome("You lose! Rock beats Scissors.");
-        return "lose"
+        displayOutCome("You lose this round! Rock beats Scissors.");
+        return "lose";
       } else{
         displayOutCome("It's a tie! You both played Scissors.");
-        return "tie"
+        return "tie";
       }
   }
 }
 
 let result;
 
-// Score Board Start
+const playerSelectionDisplay = document.getElementById('player-selection');
+const computerSelectionDisplay = document.getElementById('computer-selection');
+
+function selectionDisplay(playerSelection){
+  playerSelectionDisplay.innerText = playerSelection.toUpperCase();
+  computerSelectionDisplay.innerText = computerSelection.toUpperCase();
+}
+
 let playerScore = 0, computerScore = 0;
-const score = document.querySelector('.score-board')
-const pScore = document.createElement('p')
-const cScore = document.createElement('p')
+const pScore = document.getElementById('player-score');
+const cScore = document.getElementById('computer-score');
 
-score.appendChild(pScore);
-score.appendChild(cScore);
-
-pScore.innerText = "Player's Score: " + playerScore
-cScore.innerText = "Computer's Score: " + computerScore
+pScore.innerText = "Player's Score: " + playerScore;
+cScore.innerText = "Computer's Score: " + computerScore;
 
 function scoreBoard(){
   switch(result){
     case "win":
-      ++playerScore
+      ++playerScore;
       break;
     case "lose":
-      ++computerScore
+      ++computerScore;
       break;
   }
 
-  pScore.innerText = "Player's Score: " + playerScore
-  cScore.innerText = "Computer's Score: " + computerScore
+  pScore.innerText = "Player's Score: " + playerScore;
+  cScore.innerText = "Computer's Score: " + computerScore;
 }
-// Score Board End
 
-// Annouce Winner  Start
-const winner = document.querySelector('.winner')
-const text = document.createElement('p')
-winner.appendChild(text)
+const winner = document.querySelector('.winner');
+const modalDiv = document.querySelector('#modal');
+const playAgainButton = document.querySelector('.play-again');
 
 function announceWinner(){
+  modalDiv.classList.add('modal');
+
   if(playerScore > computerScore){
-    text.innerText = "\n*****WINNER*****" + "\n\nPlayer wins by " + (playerScore - computerScore) + " points." + "\n\nGame Over!\nReload to play again."
+    winner.innerText = "Player wins by " + (playerScore - computerScore) + " points.";
   } else if(playerScore < computerScore){
-    text.innerText = "\n*****WINNER*****" + "\n\nComputer wins by " + (computerScore - playerScore) + " points." + "\n\nGame Over!\nReload to play again."
+    winner.innerText =  "Computer wins by " + (computerScore - playerScore) + " points.";
   }
+
+  playAgainButton.addEventListener('click', () => location.reload());
 }
-// Annouce Winner End
 
 function playGame(){
-  result = playRound(this.innerText)
+  result = playRound(this.innerText);
+  selectionDisplay(this.innerText);
   scoreBoard();
 
-  if(playerScore == 5 || computerScore == 5){
-    announceWinner();
-    buttons.forEach(button => button.disabled = true)
+  if(playerScore == 10 || computerScore == 10){
+    setTimeout(announceWinner, 500);
+    buttons.forEach(button => button.disabled = true);
   }
 }
 
-const buttons = document.querySelectorAll('button')
-buttons.forEach(button => button.addEventListener('click', playGame))
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(button => button.addEventListener('click', playGame));
